40d086516c8d02bd44713aae8b6836caa20d2c02
Priyasakshi0705 <85500647+Priyasakshi0705@users.noreply.github.com>
2022-01-12 20:48:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function addUser(){
+
+    player1_name = document.getElementById("player1_name").value;
+    player2_name = document.getElementById("player2_name").value;
+
+    localStorage.setItem("player1", player1_name);
+    localStorage.setItem("player2", player2_name);
+
+    document.getElementById("player1_name").value="";
+    document.getElementById("player2_name").value="";
+    
+    window.location="math_quiz.html";
+    
+}
+
+
+
+
